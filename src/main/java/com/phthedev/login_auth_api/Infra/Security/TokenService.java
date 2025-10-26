d37b3040fa465666378ae03ff6ca6ab96d0caa4e
phthedevx <pedro.h.santos20@ba.estudante.senai.br>
2025-10-26 12:56:55 -0300

    Create TokenService

---
+++ src/main/java/com/phthedev/login_auth_api/Infra/Security/TokenService.java
@@ -0,0 +1,57 @@
+package com.phthedev.login_auth_api.Infra.Security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.phthedev.login_auth_api.Domain.User.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.secutrity.token.secret}")
+    private String secret;
+
+    public String geranateToken(User user){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = JWT.create()
+                    .withIssuer("login-auth-api")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(this.generateExpirationDate())
+                    .sign(algorithm);
+            return token;
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Error while authenticating");
+        }
+    }
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            return null;
+        }
+    }
+
+    private Instant generateExpirationDate(){
+        return LocalDateTime.now()
+                .plusHours(2)
+                .atZone(ZoneId.of("America/Bahia"))
+                .toInstant();
+
+    }
+}
