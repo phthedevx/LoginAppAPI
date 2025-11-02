c905ecd447e9a3e312b1804520517cf1f49a61e2
phthedevx <pedro.h.santos20@ba.estudante.senai.br>
2025-11-02 03:34:30 -0300

    add UserController

--- src/main/java/com/phthedev/login_auth_api/Controllers/UserController.java
+++ src/main/java/com/phthedev/login_auth_api/Controllers/UserController.java
@@ -1,4 +1,17 @@
 package com.phthedev.login_auth_api.Controllers;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    @GetMapping
+    public ResponseEntity<String> getUser(){
+        return ResponseEntity.ok("sucesso!");
+    }
+
 }
